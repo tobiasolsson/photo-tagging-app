07d0d9da190d35f8ac4c887100dbae7ce3eb508c
Tobias Olsson <tobias.olsson@pm.me>
2022-01-11 19:54:24 +0100

    Work on submit form, not usable yet

--- src/components/submitForm/SubmitForm.js
+++ src/components/submitForm/SubmitForm.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './SubmitForm.module.css';
 
 const SubmitForm = function ({ count, exitGame }) {
   const navigate = useNavigate();
+
+  function convertSeconds(s) {
+    const totalSeconds = s;
+    // const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor(totalSeconds / 60);
+    const totalMin = minutes <= 9 ? `0${minutes}` : minutes;
+    const seconds = totalSeconds % 60;
+    const totalSec = seconds <= 9 ? `0${seconds}` : seconds;
+    return `${totalMin}:${totalSec}`;
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -17,10 +28,13 @@ const SubmitForm = function ({ count, exitGame }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {count}
-      <input type="text" />
-      <button type="submit">Submit</button>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <p>You managed to find all the characters in {convertSeconds(count)}!</p>
+      <p>Enter your name to join the leaderboard:</p>
+      <input className={styles.input} type="text" />
+      <button className={styles.button} type="submit">
+        Submit
+      </button>
     </form>
   );
 };
